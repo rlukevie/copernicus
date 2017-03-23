de7cd1188b19575a30e4c83c1225143fff49b029
Roland Lukesch <r.lukesch@gmx.net>
2017-03-23 23:46:56 +0100

    finish basic functionality

--- factorytools/request.py
+++ factorytools/request.py
@@ -1,16 +1,23 @@
 import requests
+import xml.etree.cElementTree as ET
+
 from factorytools.settings import *
 
 
 class ProductSelector:
+    instances = []
+
     def __init__(self, request):
         if not isinstance(request, dict):
             raise TypeError("Requests have to be dictionaries")
         if "q" not in request:
             raise KeyError("No key 'q' in request")
         else:
+            self.__class__.instances.append(self)
             self.request = request
+            self._tree = None
             self.response = None
+            self.selected_product = None
 
     def send(self):
         logging.info('Opensearch request: {}'.format(self.request))
@@ -26,26 +33,8 @@ class ProductSelector:
         except IOError:
             pass
 
-    def parse(self):
-        pass
-
     def select(self):
-        pass
-
-
-class SelectorQueue:
-    def __init__(self):
-        self.queue = []
-
-    def append(self, request_instance):
-        """Append ProductSelector instances to queue"""
-        if not isinstance(request_instance, ProductSelector):
-            raise TypeError("Only instances of ProductSelector can be appended")
-        else:
-            self.queue.append(request_instance)
-
-    def send(self):
-        """Send all requests in queue to Open Data Hub
-        and return responses"""
-        for request in self.queue:
-            request.send()
+        # TODO: Was, wenn bei "send" None herauskommt?
+        self._tree = ET.fromstring(self.response.text)
+        ns = {'atom': 'http://www.w3.org/2005/Atom'}
+        self.selected_product = self._tree.find("./atom:entry", ns)
