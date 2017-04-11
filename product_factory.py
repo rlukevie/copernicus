94b045b8f6d54a7f6cb1ede46dc6f58549bdb28f
Roland Lukesch <r.lukesch@gmx.net>
2017-04-11 14:47:12 +0200

    finish basic factory workflow

--- product_factory.py
+++ product_factory.py
@@ -1,17 +1,18 @@
-from factorytools.product import FactoryProduct
-from factorytools.product_shelve import Shelve
-from factorytools.request import ProductSelector
-from factorytools.settings import *
+from copernicus_tools.mail import MailHandler
+from copernicus_tools.product import FactoryProduct
+from copernicus_tools.product_shelve import Shelve
+from copernicus_tools.request import ProductSelector
+from copernicus_tools.settings import *
 
 
 def main():
     logging.info('---------- STARTED product_factory.py ----------')
 
-    # wien = ProductSelector(
-    #     {"q": 'beginposition:[NOW-20MONTHS TO NOW] AND footprint:"Intersects('
-    #           '48.2000, 16.1000)" AND producttype:"S2MSI1C" AND '
-    #           'cloudcoverpercentage:[0 TO 10]',
-    #      "rows": "10"})
+    wien = ProductSelector(
+        {"q": 'beginposition:[NOW-20MONTHS TO NOW] AND footprint:"Intersects('
+              '48.2000, 16.1000)" AND producttype:"S2MSI1C" AND '
+              'cloudcoverpercentage:[0 TO 10]',
+         "rows": "10"})
 
     jiangtse_muendung = ProductSelector(
         {"q": 'beginposition:[NOW-10MONTHS TO NOW] AND '
@@ -26,22 +27,32 @@ def main():
         except IOError:
             logging.debug("IOError when sending request")
         try:
-            instance.select()
+            instance.select_first()
         except ValueError:
             logging.debug("Cannot select product: self.response = None")
         FactoryProduct(instance.selected_product)
 
     download_shelve = Shelve(downloaded_products_shelve)
+    to_analyze_shelve = Shelve(products_to_analyze_shelve)
 
-    for instance in FactoryProduct.instances:
+    admin_mail_man = MailHandler()
+
+    for product in FactoryProduct.instances:
+        admin_mail_man.send_product_selected_mail(product)
         try:
-            if not download_shelve.product_exists(instance.title):
-                instance.download_zip()
-                download_shelve.write_product(instance)
-                instance.unzip()
-                download_shelve.write_product(instance)
-                instance.process_level_1c_to_2a()
-                download_shelve.write_product(instance)
+            if not download_shelve.product_exists(product.title):
+                product.download_zip()
+                download_shelve.write_product(product)
+                admin_mail_man.send_product_downloaded_mail(product)
+
+                product.unzip()
+                download_shelve.write_product(product)
+
+                product.process_level_1c_to_2a()
+                download_shelve.write_product(product)
+                admin_mail_man.send_product_processed_mail(product)
+
+                to_analyze_shelve.write_product(product)
         except IOError as io:
             logging.debug(io.args[0])
     download_shelve.close()
