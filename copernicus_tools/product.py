d8cdba8fcf48bfdc32cf25ac5cc403f6d6d4655c
Roland Lukesch <r.lukesch@gmx.net>
2017-04-11 14:42:29 +0200

    start with product_lab

---
+++ copernicus_tools/product.py
@@ -0,0 +1,113 @@
+import xml.etree.ElementTree as ET
+import zipfile
+
+import requests
+
+from factorytools.settings import *
+
+
+class Product(object):
+    def __init__(self, selected_product):
+        if not isinstance(selected_product, ET.Element):
+            raise TypeError("Selected products have to be of type 'Element'")
+        else:
+            self.xml_tree = selected_product
+
+            ns = {'atom': 'http://www.w3.org/2005/Atom'}
+            self.id = self.xml_tree.find("./atom:id", ns).text
+            self.title = self.xml_tree.find("./atom:title", ns).text
+            self.filename = self.title + '.SAFE'
+            self.summary = self.xml_tree.find("./atom:summary", ns).text
+            self.beginposition = self.xml_tree.find(
+                "./atom:date[@name='beginposition']", ns).text
+            size_text = self.xml_tree.find("./atom:str[@name='size']", ns).text
+            self.size = self.convert_size_text_to_float(size_text)
+            self.producttype = self.xml_tree.find(
+                "./atom:str[@name='producttype']", ns).text
+            self.gmlfootprint = self.xml_tree.find(
+                "./atom:str[@name='gmlfootprint']", ns).text
+            self.cloudcoverpercentage = float(self.xml_tree.find(
+                "./atom:double[@name='cloudcoverpercentage']", ns).text)
+
+    def convert_size_text_to_float(self, size_text):
+        if "MB" in size_text:
+            return float(size_text[:-3])
+        if "GB" in size_text:
+            return float(size_text[:-3]) * 1000
+
+
+class FactoryProduct(Product):
+    instances = []
+
+    def __init__(self, xml_tree):
+        super(FactoryProduct, self).__init__(xml_tree)
+
+        self.__class__.instances.append(self)
+
+        self.zip_name = self.title + '.zip'
+        self.level_1c_name = None
+        self.level_2a_name = None
+
+    def download_zip(self):
+        download_path = os.path.join(product_download_directory, self.zip_name)
+        logging.info(
+            'Requesting download for: {} | {}'.format(self.id, self.title))
+
+        r = requests.get(
+            "https://scihub.copernicus.eu/dhus/odata/v1/Products('" +
+            self.id + "')/$value",
+            auth=(opendatahub_user, opendatahub_password),
+            stream=True)
+
+        if r.status_code == 200:
+            logging.info(
+                'Starting download for: {} | {}. Filesize: {}'.format(
+                    self.id, self.title, self.size))
+            with open(download_path, 'wb') as f:
+                chunks = 0
+                for chunk in r.iter_content(1024):
+                    chunks += 1
+                    if chunks % 1024 == 0:
+                        print(str(chunks / 1024) + 'MB')
+                    f.write(chunk)
+            logging.info(
+                'Product download complete for: {} | {}'.format(
+                    self.id, self.title))
+        else:
+            raise IOError("Cannot download zip: r.status_code != 200")
+
+    def unzip(self):
+        unzip_from_path = os.path.join(product_download_directory,
+                                       self.zip_name)
+        unzip_to_path = product_data_directory
+        try:
+            logging.debug('Zipfile to unzip: {}'.format(unzip_from_path))
+            product_zip = zipfile.ZipFile(unzip_from_path)
+            self.level_1c_name = product_zip.namelist()[0]
+            logging.info(
+                'Starting Unzip for: {} to {}'.format(
+                    self.title, unzip_to_path))
+            product_zip.extractall(unzip_to_path)
+            product_zip.close()
+            logging.info(
+                'Completed Unzip for: {}'.format(self.title))
+        except IOError:
+            return
+
+    def process_level_1c_to_2a(self):
+        if not self.level_1c_name:
+            logging.debug('Product object {} has no attribute "level_1c_name".'
+                          ' Cannot start L2A processing.')
+            return
+        try:
+            l1c_path = os.path.join(product_data_directory, self.level_1c_name)
+            logging.info(
+                'Starting L2A_Process for: {} | {}'.format(
+                    self.title, l1c_path))
+            os.system('L2A_Process ' + l1c_path)
+            self.level_2a_name = self.level_1c_name.replace('1C_', '2A_')
+            logging.info(
+                'Completed L2A_Process for: {} | {}'.format(
+                    self.title, l1c_path))
+        except IOError:
+            return
---
+++ copernicus_tools/settings.py
@@ -0,0 +1,69 @@
+import ConfigParser
+import logging
+import os
+
+
+# Credentials
+credentials = ConfigParser.ConfigParser()
+credentials.read("./config/credentials.cfg")
+opendatahub_user = credentials.get("opendatahub", "user")
+opendatahub_password = credentials.get("opendatahub", "pw")
+email_user = credentials.get("email", "user")
+email_password = credentials.get("email", "pw")
+email_host = credentials.get("email", "host")
+email_port = credentials.get("email", "port")
+
+# Directories
+configuration = ConfigParser.ConfigParser()
+configuration.read("./config/conf.cfg")
+base_directory = configuration.get("directories", "base")
+product_download_directory = os.path.join(base_directory, 'downloads')
+product_data_directory = os.path.join(base_directory, 'SAFE')
+log_directory = os.path.join(base_directory, 'log')
+shelve_directory = os.path.join(base_directory, 'etc')
+image_output_directory = os.path.join(base_directory, 'img')
+
+# Shelve Names
+downloaded_products_shelve = 'downloaded_products'
+products_to_analyze_shelve = 'products_to_analyze'
+
+# Open Data Hub
+open_data_hub_url = configuration.get("opendatahub", "url")
+
+# Mails
+admin_mail_from = configuration.get("mail", "admin_mail_from")
+admin_mail_to = configuration.get("mail", "admin_mail_to")
+
+# verbose?
+if configuration.get("logging", "verbose") == "True":
+    verbose = True
+else:
+    verbose = False
+
+# log output?
+if configuration.get("logging", "log_output") == "True":
+    log_name_out = 'product_factory_out_' + \
+                   time.strftime('%Y-%m-%d__%H_%M_%S') + '.log'
+    log_name_err = 'product_factory_err_' + \
+                   time.strftime('%Y-%m-%d__%H_%M_%S') + '.log'
+    sys.stdout = open(os.path.join(log_directory, log_name_out), 'a')
+    sys.stderr = open(os.path.join(log_directory, log_name_err), 'a')
+
+# reset product shelves?
+if configuration.get("debugging", "reset_downloadshelve") == "True":
+    downloaded_products_shelve_to_reset = True
+else:
+    downloaded_products_shelve_to_reset = False
+if configuration.get("debugging", "reset_analyzeshelve") == "True":
+    products_to_analyze_shelve_to_reset = True
+else:
+    products_to_analyze_shelve_to_reset = False
+
+# Logging
+standard_logfile = configuration.get("logging", "file")
+standard_logpath = os.path.join(log_directory, standard_logfile)
+logging.basicConfig(
+    filename=standard_logpath,
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)8s] %(message)s",
+    datefmt="%d.%m.%Y %H:%M:%S")
